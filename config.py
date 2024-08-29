1ed7fce9e49b3013510c46a162d3f03799de0898
Shkinderov Vladislav <55404536+PEPEtyam@users.noreply.github.com>
2024-08-29 16:33:14 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-TOKEN = '6632083027:AAGR3wRzkUq9yNFjkte6zff3T3M8EZ9PIzE'
-TOKEN_NEW = '6803061521:AAGrT9pgh7AOKdSO5hzE65Wj4C-3x5RyG5Y'
-openai_api_key = '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'
+TOKEN = 'YOUR_TOKEN'
+openai_api_key = 'YOUR_OPEN_AI_KEY'
 openai_api_url = 'https://api.openai.com/v1/chat/completions'
